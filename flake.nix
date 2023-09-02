6c9cfea130a2a8e2907cb1d8eaed65951c560525
micnncim <micnncim@gmail.com>
2023-09-02 18:22:35 +0900

    Add rust template
    Signed-off-by: micnncim <micnncim@gmail.com>

--- flake.nix
+++ flake.nix
@@ -12,6 +12,16 @@
             1. Run `go mod init` to initialize a go.mod file
         '';
       };
+
+      rust = {
+        path = ./rust;
+        description = "A template for Rust projects";
+        welcomeText = ''
+          Next steps:
+
+            1. Run `cargo init` to initialize a Cargo.toml file
+        '';
+      };
     };
   };
 }
---
+++ rust/flake.nix
@@ -0,0 +1,96 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/release-23.05";
+
+    flake-utils.url = "github:numtide/flake-utils";
+
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
+
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
+
+    advisory-db = {
+      url = "github:rustsec/advisory-db";
+      flake = false;
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, crane, rust-overlay, advisory-db, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ (import rust-overlay) ];
+        };
+
+        buildInputs = with pkgs; [
+          openssl
+          pkg-config
+        ] ++ lib.optionals stdenv.isDarwin [
+          libiconv
+        ];
+
+        craneLib = crane.lib.${system};
+
+        src = pkgs.lib.cleanSourceWith {
+          src = craneLib.path ./.;
+          filter = pkgs.lib.cleanSourceFilter;
+        };
+
+        cargoArtifacts = craneLib.buildDepsOnly { inherit src buildInputs; };
+
+        package = craneLib.buildPackage { inherit cargoArtifacts src buildInputs; };
+
+        rustToolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+      in
+      with pkgs;
+      {
+        checks = {
+          inherit package;
+
+          clippy = craneLib.cargoClippy {
+            inherit cargoArtifacts src buildInputs;
+            cargoClippyExtraArgs = "--all-targets -- --deny warnings";
+          };
+
+          doc = craneLib.cargoDoc { inherit cargoArtifacts src; };
+
+          fmt = craneLib.cargoFmt { inherit src; };
+
+          audit = craneLib.cargoAudit { inherit src advisory-db; };
+
+          nextest = craneLib.cargoNextest {
+            inherit cargoArtifacts src buildInputs;
+            partitions = 1;
+            partitionType = "count";
+          };
+        };
+
+        packages = {
+          inherit package;
+          default = package;
+        };
+
+        apps.default = flake-utils.lib.mkApp { drv = package; };
+
+        devShells.default = mkShell {
+          buildInputs = buildInputs ++ [
+            rustToolchain
+          ];
+
+          RUST_SRC_PATH = "${rustToolchain}/lib/rustlib/src/rust/src";
+        };
+      }
+    );
+}
