f9fc376a76531ba66d0c84193d61350bf85f5054
micnncim <micnncim@gmail.com>
2023-09-02 18:11:46 +0900

    Add go template
    Signed-off-by: micnncim <micnncim@gmail.com>

--- flake.nix
+++ flake.nix
@@ -2,6 +2,16 @@
   description = "micnncim's collection of flake templates";
 
   outputs = { self }: {
-    templates = { };
+    templates = {
+      go = {
+        path = ./go;
+        description = "A template for Go projects";
+        welcomeText = ''
+          Next steps:
+
+            1. Run `go mod init` to initialize a go.mod file
+        '';
+      };
+    };
   };
 }
---
+++ go/flake.nix
@@ -0,0 +1,30 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/release-23.05";
+
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ ];
+        };
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell {
+          buildInputs = [
+            delve
+            go
+            golangci-lint
+            gopls
+            gotest
+            gotests
+          ];
+        };
+      }
+    );
+}
